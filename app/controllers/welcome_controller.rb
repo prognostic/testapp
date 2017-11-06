b74b5e723c98d907656c7610604929385477950c
Sergey Krasnodemsky <sergeykrasnodemskiy@gmail.com>
2017-11-06 15:46:49 +0600

    add the message form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
-  def hello
+  #respond_to :html, :js
+  
+  def index; end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#hello'
-  get 'welcome/hello'
+  get 'welcome/index'
+  post 'welcome/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
